58162ca50c21dec3d455f1cf29307601658a7e48
caturic <catur.perkasa@indocyber.id>
2024-05-31 16:15:33 +0700

    added year

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	// create a simple handler
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, idhul adha!")
+		fmt.Fprint(w, "Hello, idhul adha 1444H!")
 	})
 
 	// create a html page
